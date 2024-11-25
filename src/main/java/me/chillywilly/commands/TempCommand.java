683e549a1c53252c6696f02d5a1ccbefa9011683
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-25 12:19:22 -0700

    Added more

--- src/main/java/me/chillywilly/commands/TempCommand.java
+++ src/main/java/me/chillywilly/commands/TempCommand.java
@@ -1,9 +1,15 @@
 package me.chillywilly.commands;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
 import me.chillywilly.CameraPlugin;
 
@@ -23,8 +29,41 @@ public class TempCommand implements CommandExecutor {
 
         Player player = (Player) sender;
         player.sendMessage("Packets Sent!");
-        plugin.sendMessage(player, "command.render.render-complete");
-        plugin.getNetManager().generateAndSendAuth(player);
+        Player companion = plugin.getNetManager().getCompanion();
+
+        if (companion != null && companion.isOnline()) {
+            plugin.getNetManager().screenshot(companion);
+            return true;
+        }
+
+        plugin.getNetManager().findCompanion();
+
+        BukkitTask task = Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                while ((plugin.getNetManager().getCompanion() == null) || !plugin.getNetManager().getCompanion().isOnline()) {}
+
+                plugin.getNetManager().screenshot(plugin.getNetManager().getCompanion());
+            }
+
+            
+        });
+
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+            @Override
+            public void run() {
+                if (plugin.getNetManager().getCompanion() == null || !plugin.getNetManager().getCompanion().isOnline()) {
+                    task.cancel();
+                    plugin.sendMessage(player, "command.render.no-account-online");
+
+                    plugin.getLogger().info("Companion Check: (NullCheck: " + (plugin.getNetManager().getCompanion() == null) + ")");
+                    if (companion != null) {
+                        plugin.getLogger().info("Companion Check: (Online Check: " + plugin.getNetManager().getCompanion().isOnline() + ")");
+                    }
+                }
+            }
+        }, 200);
+        
         return true;
     }
 }
