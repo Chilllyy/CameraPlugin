7b9ab64ac10696a8c96283c0d6865e79a47affcc
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-24 17:14:09 -0700

    redid main class to remove some unnecessary stuff an added NetManager references

--- src/main/java/me/chillywilly/CameraPlugin.java
+++ src/main/java/me/chillywilly/CameraPlugin.java
@@ -5,51 +5,42 @@ import java.io.File;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.chillywilly.commands.CameraCommand;
+import me.chillywilly.commands.TempCommand;
 import me.chillywilly.shoots.ShootsManager;
 import me.chillywilly.util.BuiltinMessages;
+import me.chillywilly.util.NetManager;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 
 public class CameraPlugin extends JavaPlugin {
-    public static String resource_path;
     public static String data_path;
     public static String shoots_path;
-    private static String output_path;
-
-    public static double FOV;
 
     private BuiltinMessages messages;
     private ShootsManager shoots;
+
+    private NetManager netManager;
     @Override
     public void onEnable() {
-        System.setProperty("java.awt.headless", "true");
-
-        resource_path = getDataFolder().getAbsolutePath() + File.separator + "resourcepack" + File.separator + "assets" + File.separator;
         data_path = getDataFolder().getAbsolutePath() + File.separator + "data" + File.separator;
         shoots_path = data_path + "shoots" + File.separator;
-        output_path = getDataFolder().getAbsolutePath() + File.separator + "output" + File.separator;
 
-        if (new File(resource_path).mkdirs()) {
-            getLogger().info("Successfully created resourcepack folder");
-        }
         if (new File(data_path).mkdirs()) {
             getLogger().info("Successfully created data folder");
         }
         if (new File(shoots_path).mkdirs()) {
             getLogger().info("Successfully created shoots folder");
         }
-        if (new File(output_path).mkdirs()) {
-            getLogger().info("Successfully created output folder");
-        }
 
-        saveDefaultConfig();
         saveResource("messages.yml", false);
         messages = new BuiltinMessages(this);
 
         shoots = new ShootsManager(this);
 
-        getCommand("camera").setExecutor(new CameraCommand(this));
+        //getCommand("camera").setExecutor(new CameraCommand(this));
+        getCommand("camera").setExecutor(new TempCommand(this));
+
+        netManager = new NetManager(this);
     }
 
     public void sendMessage(Player player, String key) {
@@ -70,5 +61,9 @@ public class CameraPlugin extends JavaPlugin {
     public void resetMessageCache() {
         messages.reload();
         //TODO make messages an actual cache (loads all on start)
+    }
+
+    public NetManager getNetManager() {
+        return netManager;
     }
 }
