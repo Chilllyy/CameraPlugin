8fbb0c05ab7f6b32a13f43730b5b5c8b3e28cff3
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-12-19 21:32:00 -0700

    prepping for network setup

---
+++ src/main/java/me/chillywilly/network/NetManager.java
@@ -0,0 +1,5 @@
+package me.chillywilly.network;
+
+public class NetManager {
+    
+}
---
+++ src/main/java/me/chillywilly/util/PluginConst.java
@@ -0,0 +1,13 @@
+package me.chillywilly.util;
+
+public class PluginConst {
+    public class Network {
+        //Outgoing
+        public static final String SCREENSHOT_PACKET_ID = "camera:screenshot";
+        public static final String CHECK_FOR_COMPANION_ID = "camera:companion_syn";
+
+        //Incoming
+        public static final String COMPANION_FOUND_ID = "camera:companion_ack";
+        public static final String SCREENSHOT_TAKEN_ID = "camera:uploaded";
+    }
+}
