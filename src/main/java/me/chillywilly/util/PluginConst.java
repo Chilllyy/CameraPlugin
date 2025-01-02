659c96510eb5c0658196fa5eda245efa4d73a692
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-01 20:26:39 -0700

    added some constants for the files

--- src/main/java/me/chillywilly/util/PluginConst.java
+++ src/main/java/me/chillywilly/util/PluginConst.java
@@ -1,5 +1,9 @@
 package me.chillywilly.util;
 
+import java.io.File;
+
+import me.chillywilly.CameraPlugin;
+
 public class PluginConst {
     public class Network {
         //Outgoing
@@ -9,5 +13,19 @@ public class PluginConst {
         //Incoming
         public static final String COMPANION_FOUND_ID = "camera:companion_ack";
         public static final String SCREENSHOT_TAKEN_ID = "camera:uploaded";
+    }
+
+    public class Storage {
+        public static final File data_folder = new File(CameraPlugin.plugin.getDataFolder(), "data");
+        public static final File images_folder = new File(data_folder, "images");
+        public static final File shoots_folder = new File(data_folder, "shoots");
+        public static final File web_folder = new File(data_folder, "web");
+
+        public static void init() {
+            data_folder.mkdirs();
+            images_folder.mkdirs();
+            shoots_folder.mkdirs();
+            web_folder.mkdirs();
+        }
     }
 }
