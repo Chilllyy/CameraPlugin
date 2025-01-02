1a7dfb3a3dccab02f242e2d5438dc4abfe0d12d5
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-01 20:26:33 -0700

    Created Shoot Info and Shoot Manager skeleton classes

---
+++ src/main/java/me/chillywilly/shoots/ShootInfo.java
@@ -0,0 +1,13 @@
+package me.chillywilly.shoots;
+
+import org.bukkit.Location;
+
+public class ShootInfo {
+    private String type;
+    private Location camera_location;
+    private Location sense_location;
+    private Location shoot_location; //shoot location, only for rollercoaster type
+
+    private float range;
+    private float timer;
+}
---
+++ src/main/java/me/chillywilly/shoots/ShootManager.java
@@ -0,0 +1,5 @@
+package me.chillywilly.shoots;
+
+public class ShootManager {
+    
+}
