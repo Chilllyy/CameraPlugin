da58f53fa988100d8565b4933ccdb3a93a5ec7a8
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-04 16:05:27 -0700

    redid reload and added companion manager

--- src/main/java/me/chillywilly/CameraPlugin.java
+++ src/main/java/me/chillywilly/CameraPlugin.java
@@ -12,6 +12,7 @@ public class CameraPlugin extends JavaPlugin {
 
     public static CameraPlugin plugin;
     public ShootManager shootManager;
+    private CompanionManager companionManager;
 
     @Override
     public void onEnable() {
@@ -27,10 +28,28 @@ public class CameraPlugin extends JavaPlugin {
         PluginConst.Storage.init(); //Init all storage folders and constant variables (For accessing files)
         shootManager = new ShootManager(); //Init Shoot Manager
 
-        getServer().getPluginManager().registerEvents((Listener) new CompanionManager(), this);
+        companionManager = new CompanionManager();
+        getServer().getPluginManager().registerEvents((Listener) companionManager, this);
     }
 
-    public void reload() {
-        shootManager.reload();
+    public void reload(int reload) {
+        /*
+         * 0: Reload All
+         * 1: Reload Core
+         * 2: Reload Web
+         */
+        switch (reload) {
+            case 0:
+                shootManager.reload();
+                companionManager.reload();
+                break;
+            case 1:
+                shootManager.reload();
+                companionManager.reload();
+                break;
+            case 2:
+                //TODO reload web
+                break;
+        }
     }
 }
