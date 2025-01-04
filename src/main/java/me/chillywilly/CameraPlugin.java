3908f100f62752bee345c96392b37c09a8d0fc7f
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-04 12:49:18 -0700

    added reload method and shoot manager

--- src/main/java/me/chillywilly/CameraPlugin.java
+++ src/main/java/me/chillywilly/CameraPlugin.java
@@ -3,11 +3,13 @@ package me.chillywilly;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.chillywilly.command.CameraCommand;
+import me.chillywilly.shoots.ShootManager;
 import me.chillywilly.util.PluginConst;
 
 public class CameraPlugin extends JavaPlugin {
 
     public static CameraPlugin plugin;
+    public ShootManager shootManager;
 
     @Override
     public void onEnable() {
@@ -21,5 +23,10 @@ public class CameraPlugin extends JavaPlugin {
         CameraPlugin.plugin = this;
 
         PluginConst.Storage.init(); //Init all storage folders and constant variables (For accessing files)
+        shootManager = new ShootManager(); //Init Shoot Manager
+    }
+
+    public void reload() {
+        shootManager.reload();
     }
 }
