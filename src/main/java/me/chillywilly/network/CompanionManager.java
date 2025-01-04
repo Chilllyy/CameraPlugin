6191d19433a74e67dd097615477b83233d77d26a
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-04 15:31:25 -0700

    added join event

--- src/main/java/me/chillywilly/network/CompanionManager.java
+++ src/main/java/me/chillywilly/network/CompanionManager.java
@@ -1,5 +1,15 @@
 package me.chillywilly.network;
 
-public class CompanionManager {
+import java.net.http.WebSocket.Listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class CompanionManager implements Listener {
     
+
+    @EventHandler
+    public void joinEvent(PlayerJoinEvent event) {
+        
+    }
 }
